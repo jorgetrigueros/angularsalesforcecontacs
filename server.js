86a11addfd7425d0b8ca0e99bd924c621da309a7
Jorge Trigueros <jorge.trigueros@ticmind.es>
2020-11-16 13:38:03 +0100

    updates deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angularsalesforcesample'));
+app.use(express.static(__dirname + '/dist/ngx-salesforce-contacts'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angularsalesforcesample/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ngx-salesforce-contacts/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
